03e77345b12498f1b454cb0ade84062a5850fb11
Hyunoh Sung <harrysho@gmail.com>
2024-11-05 15:57:31 +0900

    main2

--- pyqt_practice/main.py
+++ pyqt_practice/main.py
@@ -14,11 +14,46 @@ class Main(QDialog):
         layout = QVBoxLayout()
 
         label_widget = QLabel("Hello World!")
+        button_widget = QPushButton("Click Me")
+        combobox_widget = QComboBox()
+        combobox_widget.addItem("Python1")
+        combobox_widget.addItem("Python2")
+        combobox_widget.addItem("Python3")
+        combobox_widget.addItem("Python4")
+        combobox_widget.addItem("Python5")
+
+        check_box_widget_1 = QCheckBox("Summer")
+        check_box_widget_2 = QCheckBox("Winter")
+
+        radio_button_widget_male = QRadioButton("Male")
+        radio_button_widget_female = QRadioButton("Female")
+
+        spinbox_widget = QSpinBox()
+
+        date_widget = QDateEdit()
+        time_widget = QTimeEdit()
+
+        list_widget = QListWidget()
+        item_1 = QListWidgetItem("Dog")
+        item_2 = QListWidgetItem("Cat")
+        list_widget.addItem(item_1)
+        list_widget.addItem(item_2)
 
         layout.addWidget(label_widget)
+        layout.addWidget(button_widget)
+        layout.addWidget(combobox_widget)
+        layout.addWidget(check_box_widget_1)
+        layout.addWidget(check_box_widget_2)
+        layout.addWidget(radio_button_widget_male)
+        layout.addWidget(radio_button_widget_female)
+        layout.addWidget(spinbox_widget)
+        layout.addWidget(date_widget)
+        layout.addWidget(time_widget)
+        layout.addWidget(list_widget)
+
 
         self.setLayout(layout)
-
+        self.resize(500,500)
         self.show()
 
 if __name__ == '__main__':
